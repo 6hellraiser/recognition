f421341c6e15789bfef56edf604e6a8267f4aefa
6hellraiser <nataligavrilova8@gmail.com>
2014-05-05 22:12:38 +0400

    recognition of simple digits

--- advanced_descriptor.py
+++ advanced_descriptor.py
@@ -55,12 +55,14 @@ class Descriptor:
         Descriptor.horizontal_center = first_feature
         Descriptor.box_width = third_feature
         features = [first_feature, third_feature]
+        #print "first ",first_feature
+        #print "third ",third_feature
         return features
 
     def second_fourth(self, matrix):
         #2. The vertical position, counting pixels from the bottom, of the above box.
         #4. The height, in pixels, of the box.
-        print matrix
+        #print matrix
         rows_count = matrix.shape[0]
         columns_count = matrix.shape[1]
         bottom_edge = -1
@@ -90,6 +92,8 @@ class Descriptor:
         Descriptor.vertical_center = second_feature
         Descriptor.box_height = fourth_feature
         features = [second_feature, fourth_feature]
+       # print "second ",second_feature
+       # print "fourth ",fourth_feature
         return features
 
     def fifth(self, matrix):
@@ -103,6 +107,7 @@ class Descriptor:
                 if matrix[row, col] == Descriptor.black:
                     count += 1
         Descriptor.on_pixels = count
+       # print "fifth ",self.roundd(float(count)/(Descriptor.box_width*Descriptor.box_height))
         return self.roundd(float(count)/(Descriptor.box_width*Descriptor.box_height))
 
     def sixth(self, matrix): ######################################################################333debug
@@ -123,7 +128,7 @@ class Descriptor:
             x += 1
         sixth_feature = float(mean_horizontal)/(Descriptor.on_pixels * Descriptor.box_width)
     #    print "mean horizontal", mean_horizontal
-    #    print "sixth feature", sixth_feature
+        #print "sixth feature", self.roundd(sixth_feature)
         return self.roundd(sixth_feature) #very little number!
 
     def seventh(self, matrix):
@@ -142,6 +147,7 @@ class Descriptor:
             y = 0
             x += 1
         seventh_feature = float(mean_vertical)/(Descriptor.on_pixels * Descriptor.box_height)
+       # print "seventh ", self.roundd(seventh_feature)
         return self.roundd(seventh_feature)
 
     def eighth(self, matrix):
@@ -154,8 +160,9 @@ class Descriptor:
         for col in range(columns_count):
             for row in range(rows_count):
                 if matrix[row, col] == Descriptor.black:
-                    quadr_sum += ((Descriptor.horizontal_center - col)/Descriptor.box_width)**2
+                    quadr_sum += (float(Descriptor.horizontal_center - col)/Descriptor.box_width)**2
         eighth_feature = math.sqrt(float(quadr_sum)/Descriptor.on_pixels)
+       # print "eighth ", self.roundd(eighth_feature)
         return self.roundd(eighth_feature)
 
     def nineth(self, matrix):
@@ -166,8 +173,9 @@ class Descriptor:
         for col in range(columns_count):
             for row in range(rows_count):
                 if matrix[row, col] == Descriptor.black:
-                    quadr_sum += ((Descriptor.vertical_center - row)/Descriptor.box_height)**2
+                    quadr_sum += (float(Descriptor.vertical_center - row)/Descriptor.box_height)**2
         nineth_feature = math.sqrt(float(quadr_sum)/Descriptor.on_pixels)
+        #print "nineth ",self.roundd(nineth_feature)
         return self.roundd(nineth_feature)
 
     def tenth(self, matrix):
@@ -182,11 +190,13 @@ class Descriptor:
         y = 0 - Descriptor.box_height/2
         for col in range(columns_count):
             for row in range(rows_count):
-                if matrix[row, col] == Descriptor.color:
+                if matrix[row, col] == Descriptor.black:
                     sum += x*y
-                x += 1
-            y += 1
-        return self.roundd(sum/(Descriptor.on_pixels*Descriptor.box_width*Descriptor.box_height))
+                y += 1
+            y = 0
+            x += 1
+        #print "tenth ", self.roundd(float(sum)/Descriptor.on_pixels)
+        return self.roundd(float(sum)/Descriptor.on_pixels)
 
     def eleventh_twelfth(self, matrix):
         #11. The mean value of the squared horizontal distance times the vertical distance for each
@@ -203,13 +213,16 @@ class Descriptor:
         y = 0 - Descriptor.box_height/2
         for col in range(columns_count):
             for row in range(rows_count):
-                if matrix[row, col] == Descriptor.color:
-                    sum_gor += ((x/Descriptor.box_width)**2)*y
-                    sum_vert += ((y/Descriptor.box_height)**2)*x
-                x += 1
-            y += 1
-        eleventh_feature = self.roundd(sum_gor/(Descriptor.on_pixels*Descriptor.box_height))
-        twelfth_feature = self.roundd(sum_vert/(Descriptor.on_pixels*Descriptor.box_width))
+                if matrix[row, col] == Descriptor.black:
+                    sum_gor += ((float(x)/Descriptor.box_width)**2)*y
+                    sum_vert += ((float(y)/Descriptor.box_height)**2)*x
+                y += 1
+            x = 0
+            x += 1
+        eleventh_feature = self.roundd(float(sum_gor)/Descriptor.on_pixels)##*Descriptor.box_height))
+        twelfth_feature = self.roundd(float(sum_vert)/Descriptor.on_pixels)##*Descriptor.box_width))
+       # print "eleventh ", eleventh_feature
+       # print "twelfth ", twelfth_feature
         features = [eleventh_feature, twelfth_feature]
         return features
 
@@ -229,14 +242,17 @@ class Descriptor:
         vert_sum = 0
         for col in range(columns_count):
             for row in range(rows_count):
-                if matrix[row, col] == Descriptor.color:
-                    if (col-1) < 0 or matrix[row, col - 1] != Descriptor.color:
+                if matrix[row, col] == Descriptor.black:
+                    if (col-1) < 0 or matrix[row, col - 1] != Descriptor.black:
                         count_edges += 1
                         vert_sum += y
-            y += 1
-        thirteen_feature = self.roundd(count_edges/(Descriptor.box_height*Descriptor.box_width))
-        fourteen_feature = self.roundd(vert_sum/Descriptor.box_height) #??????????????????????
+                y += 1
+            y = 0
+        thirteen_feature = self.roundd(float(count_edges)/Descriptor.on_pixels)
+        fourteen_feature = self.roundd(float(vert_sum)/Descriptor.box_height)
         features = [thirteen_feature, fourteen_feature]
+       # print "thirteen " ,thirteen_feature
+       # print "fourteen " ,fourteen_feature
         return features
 
     def fifteen_sixteen(self, matrix):
@@ -251,14 +267,16 @@ class Descriptor:
         hor_sum = 0
         for col in range(columns_count):
             for row in range(rows_count):
-                if matrix[row, col] == Descriptor.color:
-                    if (row + 1) == rows_count or matrix[row + 1, col] != Descriptor.color:
+                if matrix[row, col] == Descriptor.black:
+                    if (row + 1) == rows_count or matrix[row + 1, col] != Descriptor.black:
                         count_edges += 1
                         hor_sum += x
-                x += 1
-        fifteen_feature = self.roundd(count_edges/(Descriptor.box_width*Descriptor.box_height))
-        sixteen_feature = self.roundd(hor_sum/Descriptor.box_width) #???????????????????????
+            x += 1
+        fifteen_feature = self.roundd(float(count_edges)/Descriptor.on_pixels)
+        sixteen_feature = self.roundd(float(hor_sum)/Descriptor.box_width) #???????????????????????
         features = [fifteen_feature, sixteen_feature]
+       # print "fifteen ", fifteen_feature
+       # print "sixteen ", sixteen_feature
         return features
 
     def descriptor(self, matrix):
@@ -285,6 +303,7 @@ class Descriptor:
         fifteen = self.fifteen_sixteen(matrix)
         for el in fifteen:
             vector.append(el)
+      #  print vector
         return vector
 
 def predict(path, clf, input):
@@ -308,30 +327,27 @@ def main():
 
     thresh = 127
     vectors = []
-    """
     images = [cv2.imread('new_learn/0_1.png',0), cv2.imread('new_learn/0_2.png',0), cv2.imread('new_learn/0_3.png',0), cv2.imread('new_learn/0_4.png',0),
               cv2.imread('new_learn/0_5.png',0), cv2.imread('new_learn/0_6.png',0),cv2.imread('new_learn/0_7.png',0), cv2.imread('new_learn/0_8.png',0),
               cv2.imread('new_learn/4_1.png',0),cv2.imread('new_learn/4_2.png',0),cv2.imread('new_learn/4_3.png',0),cv2.imread('new_learn/4_4.png',0),
               cv2.imread('new_learn/4_5.png',0),cv2.imread('new_learn/4_6.png',0), cv2.imread('new_learn/4_7.png',0),cv2.imread('new_learn/4_8.png',0),
               cv2.imread('new_learn/5_1.png',0),cv2.imread('new_learn/5_2.png',0),cv2.imread('new_learn/5_3.png',0),cv2.imread('new_learn/5_4.png',0),
               cv2.imread('new_learn/5_6.png',0),cv2.imread('new_learn/5_7.png',0),cv2.imread('new_learn/5_8.png',0)]
-    """
-    images = [cv2.imread('new_learn/0_1.png',0)]
+   # images = [cv2.imread('new_learn/0_1.png',0)]
     for el in images:
         binary_matrix = cv2.threshold(el, thresh, Descriptor.color, cv2.THRESH_BINARY)[1]
         #cv2.imshow('digit', binary_matrix)
         #cv2.waitKey(0)
-       # linear_vector = descriptor(binary_matrix)
         d = Descriptor()
-        linear_vector = d.descriptor(cv2.threshold(cv2.resize(binary_matrix,(8,10)),thresh,Descriptor.color,cv2.THRESH_BINARY)[1]) #test! comment this and uncommend previous string!!
+        linear_vector = d.descriptor(binary_matrix)
+        #linear_vector = d.descriptor(cv2.threshold(cv2.resize(binary_matrix,(8,10)),thresh,Descriptor.color,cv2.THRESH_BINARY)[1]) #test! comment this and uncommend previous string!!
         vectors.append(linear_vector)
 
-   # y = [0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5]
+    y = [0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5]
    # y = [0]
-   # clf = svm.SVC()
-   # clf.fit(vectors, y)
+    clf = svm.SVC()
+    clf.fit(vectors, y)
 
-    """
     print 'expect 0, get: '
     predict('new_ask/0_t1.png',clf,'0')
     print 'expect 0, get: '
@@ -369,7 +385,6 @@ def main():
     predict('error_ask/4_t3.png',clf,'4')
     print 'expect 5, get: '
     predict('error_ask/5_t1.png',clf,'5')
-    """
 
 if __name__ == '__main__':
     main()
